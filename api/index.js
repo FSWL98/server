af2584590dafbe10ded11eda47293bacd5ee5a62
FSWL <fswl98@gmail.com>
2019-12-20 04:23:28 +0700

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,25 @@
+import config from 'dotenv'
+import express from 'express'
+import bodyParser from 'body-parser'
+import routes from './server/routes/routes'
+
+config.config();
+
+const port = 3001;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use('/api/v1/favorites', routes);
+
+app.get('*', (request, response) => {
+    console.log(`URL: ${request.url}`);
+    response.send('Hello World');
+});
+const server = app.listen(port, (error) => {
+    if (error) return console.log(`Error: ${error}`);
+    console.log(`Server is listening on port ${server.address().port}`)
+});
+
+export default app;
